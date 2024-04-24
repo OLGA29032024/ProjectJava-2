2a8cf151149854b31b77348a3445a8cf8b82d15a
Qamid <ivanova3487@gmail.com>
2024-04-25 00:34:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        int anount = 15000;
+        boolean isRegistered = true;
+
+        int mile = 20;
+
+        int bonus = anount / mile;
+
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
